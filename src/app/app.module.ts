647c3a7550ed06bbe77935b5774d08e426e6e5af
Phap <centralparkloop@yahoo.com>
2020-01-21 21:41:09 -0500

    create hamburger component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HamburgerComponent } from './hamburger/hamburger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HamburgerComponent
   ],
   imports: [
     BrowserModule,
